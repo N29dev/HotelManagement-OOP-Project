8ee932f84c25356dc8fecae222dfa712aa327d03
Nurlybek <255836@astanait.edu.kz>
2025-12-19 22:09:58 +0500

    123

---
+++ src/main/java/me/n29/Main.java
@@ -0,0 +1,13 @@
+package me.n29;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Hotel Management System!");
+        System.out.println("This system will help manage rooms, bookings, and guests.");
+        System.out.println("Features planned:");
+        System.out.println("- Manage reservations");
+        System.out.println("- Track room availability");
+        System.out.println("- Guest records");
+        System.out.println("Developed by Nurlybek Yesbolganov, OOP Course.");
+    }
+}
